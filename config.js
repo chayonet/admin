f15c12a72586c3596a9177f8d90a3245151508c7
chayonet <chayocc2026@gmail.com>
2026-04-07 22:20:23 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+/* ==========================================
+   ARCHIVO: config.js
+   Variable global para conectar toda la app
+========================================== */
+const API_BASE_URL_F = "https://apis-yley.onrender.com";
