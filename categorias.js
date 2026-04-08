67e82b2267c1825344d7e89204cd6b905b9914ab
chayonet <chayocc2026@gmail.com>
2026-04-07 21:44:10 -0500

    Update categorias.js

--- categorias.js
+++ categorias.js
@@ -1,4 +1,4 @@
-const API_ADMIN_MODULE_CAT = 'https://api-ventas-zzd6.onrender.com/admin_api.php';
+const API_ADMIN_MODULE_CAT = 'https://apis-yley.onrender.com/admin_api.php';
 let categoriaEditandoID = null;
 let categoriasInicializado = false; 
 let listaUsuariosGlobal = []; 
