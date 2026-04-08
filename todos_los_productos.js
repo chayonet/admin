5decf1828efe3ca20e01004429ec2e16f0e28638
chayonet <chayocc2026@gmail.com>
2026-04-08 00:24:04 -0500

    Update todos_los_productos.js

--- todos_los_productos.js
+++ todos_los_productos.js
@@ -1483,7 +1483,7 @@ function initEventosProductos() {
                 cuentasAgrupadas[srv].push(prod.cuenta);
             });
 
-            let contenido = "🌟 CHAYONET - INVENTARIO 🌟\r\n";
+            let contenido = `🌟 ${NOMBRE_NEGOCIO} - INVENTARIO 🌟\r\n`;
             contenido += "=======================================================\r\n\r\n";
             
             for (const srv in cuentasAgrupadas) {
